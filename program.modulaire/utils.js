7272d15a6ecf6582af899204a2b8ee9c734cd410
yelmouss <yelmouss.devt@gmail.com>
2024-11-28 20:47:33 +0100

    Update database.json with new user entry; format index.js and utils.js for consistency

--- program.modulaire/utils.js
+++ program.modulaire/utils.js
@@ -1,13 +1,13 @@
-console.log("debut utils")
-const PI = 22 / 7
-const PORT = 3000
-const somme = (a, b) => a + b
-const admin = { nom: "ali", age: 45 }
-console.log("fin utils")
+console.log("debut utils");
+const PI = 22 / 7;
+const PORT = 3000;
+const somme = (a, b) => a + b;
+const admin = { nom: "ali", age: 45 };
+console.log("fin utils");
 
-module.exports ={
-    pi : PI,
-    port : PORT,
-    somme,// somme:somme
-    admin
-}
+module.exports = {
+  pi: PI,
+  port: PORT,
+  somme, // somme:somme
+  admin,
+};
